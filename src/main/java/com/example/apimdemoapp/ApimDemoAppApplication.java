3a5f5faf328afa49af1d9df9924bfccc32d4ecef
Tolga Mert <tolgamrt@gmail.com>
2023-08-23 17:25:53 +0200

    Demo App

---
+++ src/main/java/com/example/apimdemoapp/ApimDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.apimdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimDemoAppApplication.class, args);
+	}
+
+}
